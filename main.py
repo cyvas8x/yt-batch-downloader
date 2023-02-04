e990bd91bfa35121bd2322442640150aa93904e4
cyvas8x <44058169+cyvas8x@users.noreply.github.com>
2023-02-05 06:17:49 +0700

    Rewrite Code

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-import subprocess
-#
+import youtube_dl
+from youtube_dl.utils import ytdl_is_updateable
+import yt_dlp
+
 links = open('links.txt', 'r')
-# # with open('links.txt', 'r') as file:
-#     # print(file.readlines())
-#
 urls = links.readlines()
-#
-for url in urls:
-    subprocess.call(['youtube-dl', '-x', '--audio-format', 'mp3', '-o', '%(title)s.%(ext)s', url])
 
-# from pytube import YouTube
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+print("Pilih opsi dibawah\n")
+print("1) Download MP3")
+print("2) Download MP4")
 
-#list of URLs
+# TODO: ADD english Language
+# TODO: ADD Download support for MP4
+# TODO: Cleaning Code
 
-# from yt_dlp import *
-#
-# #list of URLs
-#
-# urls = links.readlines()
-# links = open('links.txt', 'r')
-#
-# for url in urls:
-#     yt_dlp.downloader(url, format='mp3')
+while True:
+    choice = input("> ")
+    if choice.isdigit():
+        choice = int(choice)
+        if choice == 1:
+            print(input("Pastekan Link nya di file 'links.txt'\n Jika sudah klik Enter "))
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(urls)
+        elif choice == 2:
+            print("Coming Soon :)")
+            break
+        else:
+            print("Pilih 1 atau 2. Jangan pilih yang lain :3")
+    else:
+        print("Masukkan Nomer bro")
